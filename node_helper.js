87924488324ea0aa65467d093cd079aef041f03d
PK Shiu <pk@pkshiu.com>
2021-05-12 16:30:09 -0400

    fetching comments as well

--- node_helper.js
+++ node_helper.js
@@ -41,26 +41,60 @@ module.exports = NodeHelper.create({
 
   breakDownChannelIds: function () {
     this.url =
-      "https://youtube.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=";
-    this.channelIds.forEach((channel) => {
+      "https://youtube.googleapis.com/youtube/v3/channels?part=statistics&part=snippet";
+
+    let comment_url =
+      "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=3&allThreadsRelatedToChannelId="
+
+
+      this.channelIds.forEach((channel) => {
       this.url = this.url + "&id=" + channel.id;
+
     });
     this.getData(this.url);
+
+    // get comments
+    this.channelIds.forEach((channel) => {
+      this.getData2(comment_url + channel.id)
+    });
+
   },
 
-  getData: function (url) {
+  getData2: function (url) {
+    console.log(`getData for ${url}`)
     fetch(url + "&key=" + this.apiKey, {
       method: "GET",
       headers: {
         Accept: "application/json"
       }
     })
-      .then((response) => response.json())
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(`data2 text: **${JSON.stringify(data)}**`)
+      for (var i=0; i < data.items.length; i++) {
+        // TODO: Need to search for the right snippet, not always top level...
+        console.log(`item: ${data.items[i].snippet.topLevelComment.snippet.textDisplay}`)
+      }
+    })
+      .catch((error) => console.log("Error: ", error));
+  },
+
+
+  getData: function (url) {
+    console.log(`getData for ${url}`)
+    fetch(url + "&key=" + this.apiKey, {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then((response) => response.json())
       .then((data) => this.handleData(data))
       .catch((error) => console.log("Error: ", error));
   },
 
   handleData: function (data) {
+    console.log(`response data: ${data}`);
     this.sendSocketNotification("MMM-YT-SubCount-DATA_IS_READY", data);
   }
 });
